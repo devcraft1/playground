c23c9df696046f2228edaee6fa9edcca7d09a279
devcraft1 <ukonudennis64@gmail.com>
2022-06-07 22:59:13 -0900

    structures

---
+++ structures/StructWithMapping.sol
@@ -0,0 +1,44 @@
+pragma solidity ^0.8.0;
+
+contract StructWithMapping {
+    struct Test {
+        uint256 myUint;
+        string myString;
+    }
+
+    mapping(uint256 => Test) public myMapping;
+
+    function setMapping(
+        uint256 key,
+        uint256 _myUint,
+        string memory _myString
+    ) public {
+        Test memory test;
+        test.myUint = _myUint;
+        test.myString = _myString;
+        myMapping[key] = test;
+    }
+
+    function setOnlyUint(uint256 _key, uint256 _setValue) public {
+        myMapping[_numb].myUint = _setValue;
+    }
+
+    function setOnlyString(uint256 _key, string calldata _setString) public {
+        myMapping[_key].myString = _setString;
+    }
+
+    function getUintValue(uint256 _key) public view returns (uint256) {
+        uint256 uintValue = myMapping[_key].myUint;
+        return uintValue;
+    }
+
+    function getStringValue(uint256 _str) public view returns (string memory) {
+        string memory str = myMapping[_str].myString;
+        return str;
+    }
+
+    function nullifyStruct(uint256 key) public {
+        Test memory test;
+        myMapping[key] = test;
+    }
+}
