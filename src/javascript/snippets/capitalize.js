718aeb3c5d6985e2c9676c5e812030d664723b79
devcraft1 <ukonudennis64@gmail.com>
2023-01-25 21:56:09 -1100

    --javascript:capitalize

--- src/javascript/snippets/capitalize.js
+++ src/javascript/snippets/capitalize.js
@@ -1,3 +1,11 @@
+// capitalize all the words in a string
+function capitalizeAll(str) {
+    return str.replace(/\w\S*/g, function (txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+}
+
+console.log(capitalizeAll('string'));
 
 // Capitalize first letter 
 function capitalize(str) {
