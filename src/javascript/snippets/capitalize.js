6a24adb6572e965e325d9f48d36739b6e069d7ee
devcraft1 <ukonudennis64@gmail.com>
2023-01-25 22:05:42 -1100

    --javascript:capitalize

--- src/javascript/snippets/capitalize.js
+++ src/javascript/snippets/capitalize.js
@@ -2,10 +2,11 @@
 function capitalizeAll(str) {
     return str.toUpperCase()
 }
+console.log(capitalizeAll('string'));
+
 
 // Capitalize first letter 
 function capitalizeFirstLetter(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
-console.log(capitalizeAll('string'));
 console.log(capitalizeFirstLetter('string'));
