df4f2e9976eaad3f83d74d9bdd6bfddfb172b9ee
devcraft1 <ukonudennis64@gmail.com>
2023-01-25 22:21:51 -1100

    --javascript:typeOf

---
+++ src/javascript/snippets/typeOf.js
@@ -0,0 +1,23 @@
+//typeof() will return the type of value in its parameters.
+//some examples of types: undefined, NaN, number, string, object, array
+
+//example of a practical usage
+function checkType(value) {
+  if (typeof (value) === "string") return "string";
+  if (typeof (value) === "number") return "number";
+  if (typeof (value) === "boolean") return "boolean";
+  if (typeof (value) === "undefined") return "undefined";
+  if (typeof (value) === "object") return "object";
+  if (typeof (value) === "function") return "function";
+  if (typeof (value) === "symbol") return "symbol";
+  if (typeof (value) === "bigint") return "bigint";
+}
+
+console.log(checkType("string"));
+console.log(checkType(true));
+console.log(checkType(1));
+console.log(checkType(undefined));
+console.log(checkType({}));
+console.log(checkType(() => { }));
+console.log(checkType(Symbol()));
+console.log(checkType(BigInt(1)));
