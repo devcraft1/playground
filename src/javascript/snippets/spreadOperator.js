3cca0752771bd6a898ee08e76a00ed7b3a925db0
devcraft1 <ukonudennis64@gmail.com>
2023-01-26 23:29:10 -1100

    --javascript:spread operators

--- src/javascript/snippets/spreadOperator.js
+++ src/javascript/snippets/spreadOperator.js
@@ -1,3 +1,4 @@
+// Spread operators for Array
 // string
 const name = 'Zelda';
 const letters = [...name];
@@ -7,3 +8,17 @@ console.log(letters);
 const amount = [100000, 10, 20];
 const numbers = [...amount];
 console.log(numbers);
+
+
+// Spread operators for Object
+// string
+
+const person = {
+    name: 'Zelda',
+    age: 20,
+};
+const person2 = {
+    ...person,
+    name: 'Link',
+};
+console.log(person2);
