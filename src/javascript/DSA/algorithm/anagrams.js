f64976cc61284b2b3ccf98599c128842a9b61e28
devcraft1 <ukonudennis64@gmail.com>
2023-01-26 23:45:29 -1100

    --javascript:DSA-algorith-anagrams

--- src/javascript/DSA/algorithm/anagrams.js
+++ src/javascript/DSA/algorithm/anagrams.js
@@ -1,9 +1,13 @@
-function anagrams(stringA, stringB) {
-  return cleanString(stringA) === cleanString(stringB);
-}
+// function anagrams(stringA, stringB) {
+//   return cleanString(stringA) === cleanString(stringB);
+// }
 
 function cleanString(str) {
   return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+}
+
+function anagrams(stringA, stringB) {
+  return cleanString(stringA) === cleanString(stringB);
 }
 
 console.log(anagrams('string', 'string'));
