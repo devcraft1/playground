b8b2ddfdbc3613d050fb7cbca57f1c486a8d7eb3
devcraft1 <ukonudennis64@gmail.com>
2023-02-04 07:26:01 -1100

    --node: auth

--- src/node/express/auth/03-sessions-with-db/db.js
+++ src/node/express/auth/03-sessions-with-db/db.js
@@ -5,5 +5,5 @@ const mongodb = require("mongodb")
 mongodb.connect(process.env.CONNECTIONSTRING, { useUnifiedTopology: true }, function (err, client) {
   module.exports = client
   const app = require("./app.js")
-  app.listen(process.env.PORT || 3000)
+  app.listen(`http://localhost:${process.env.PORT}` || `http://localhost:${3000}`)
 })
